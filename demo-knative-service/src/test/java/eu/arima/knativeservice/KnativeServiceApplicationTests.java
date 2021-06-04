b34b1051db0f2bcf3f6fd0d97ee8a64d8228142d
Iñigo Telleria <inigo@arima.eu>
2021-06-04 11:18:11 +0200

    Add knative service project

---
+++ demo-knative-service/src/test/java/eu/arima/knativeservice/KnativeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.arima.knativeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnativeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
