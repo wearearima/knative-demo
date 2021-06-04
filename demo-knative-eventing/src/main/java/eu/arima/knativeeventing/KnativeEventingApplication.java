b9d3e1c16f968343885a1cd448e4486606ad0c4b
Iñigo Telleria <inigo@arima.eu>
2021-06-04 12:26:30 +0200

    Add knative eventing project

---
+++ demo-knative-eventing/src/main/java/eu/arima/knativeeventing/KnativeEventingApplication.java
@@ -0,0 +1,37 @@
+package eu.arima.knativeeventing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
+
+@SpringBootApplication
+public class KnativeEventingApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(KnativeEventingApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnativeEventingApplication.class, args);
+	}
+
+	@Bean
+	Consumer<String> eventConsumer(@Value("${app.delay}") int delay) {
+		return (data) -> { this.process(data, delay); };
+	}
+
+	private void process(String data, int delay) {
+
+		try {
+			Thread.sleep(delay);
+		} catch (InterruptedException e) {
+			throw new IllegalStateException("Unexpected error sleeping", e);
+		}
+
+		LOGGER.info("Event processed: {}", data);
+	}
+
+}
---
+++ demo-knative-eventing/src/test/java/eu/arima/knativeeventing/KnativeEventingApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.arima.knativeeventing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnativeEventingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
